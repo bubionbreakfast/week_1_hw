add256dee706cba9079551ad6933f5f55ca9787d
Cameron Pellett <bubionbreakfast@gmail.com>
2019-08-03 10:09:09 +0100

    tests passed up to 5th function

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,8 +1,19 @@
 def pet_shop_name(name)
-return @pet_shop[:name]
+  return @pet_shop[:name]
 end
 
 def total_cash(sum)
   return @pet_shop[:admin][:total_cash]
+end
 
+def add_or_remove_cash(cash, total_cash)
+  return @pet_shop[:admin][:total_cash] += 10
+end
+
+def add_or_remove_cash(cash, total_cash)
+  return @pet_shop[:admin][:total_cash] -= 10
+end
+
+def pets_sold(sold)
+  return @pet_shop[:admin][:pets_sold]
 end
