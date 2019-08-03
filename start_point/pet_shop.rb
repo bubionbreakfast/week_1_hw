1b5f4ac3ee9c9ecc9957a2f6036c6706e7453676
Cameron Pellett <bubionbreakfast@gmail.com>
2019-08-03 14:46:58 +0100

    up to pets by breed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,27 +1,45 @@
 def pet_shop_name(name)
-  return @pet_shop[:name]
+  return name[:name]
 end
 
 def total_cash(sum)
-  return @pet_shop[:admin][:total_cash]
+  return sum[:admin][:total_cash]
 end
 
 def add_or_remove_cash(cash, total_cash)
-  return @pet_shop[:admin][:total_cash] += 10
+  return cash[:admin][:total_cash] += 10
 end
 
 def add_or_remove_cash(cash, total_cash)
-  return @pet_shop[:admin][:total_cash] -= 10
+  return cash[:admin][:total_cash] -= 10
 end
 
 def pets_sold(sold)
-  return @pet_shop[:admin][:pets_sold]
+  return sold[:admin][:pets_sold]
 end
 
 def increase_pets_sold(sold, pets_sold)
-  return @pet_shop[:admin][:pets_sold] += 2
+  return sold[:admin][:pets_sold] += 2
 end
 
 def stock_count(count)
-  return @pet_shop[:pets].count
+  return count[:pets].count
+end
+
+def pets_by_breed(pets_hash, breed_type)
+  total_pets_by_breed = []
+  p pets_hash[:pets]
+  p pets_hash[:pets].count
+
+  for pet in pets_hash[:pets]
+    if pet[:breed_type] == breed_type
+      # total_pets_by_breed.push(pet)
+    # p pet
+    # total_pets_by_breed.push(pet)
+    p total_pets_by_breed
+    return total_pets_by_breed.count
+
+    p total_pets_by_breed.length
+  end
+end
 end
