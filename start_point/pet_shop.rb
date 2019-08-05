57280d68e2eb6c5d3e8864a147f8b369b89e79f6
Cameron Pellett <bubionbreakfast@gmail.com>
2019-08-05 11:13:48 +0100

    refactored after debrief

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -6,20 +6,17 @@ def total_cash(sum)
   return sum[:admin][:total_cash]
 end
 
-def add_or_remove_cash(cash, total_cash)
-  return cash[:admin][:total_cash] += 10
+def add_or_remove_cash(pet_shop, amount)
+   pet_shop[:admin][:total_cash] += amount
 end
 
-def add_or_remove_cash(cash, total_cash)
-  return cash[:admin][:total_cash] -= 10
-end
 
 def pets_sold(sold)
   return sold[:admin][:pets_sold]
 end
 
 def increase_pets_sold(sold, pets_sold)
-  return sold[:admin][:pets_sold] += 2
+  return sold[:admin][:pets_sold] += pets_sold
 end
 
 def stock_count(count)
@@ -36,15 +33,17 @@ for pet in pets_hash[:pets]
   return pets
 end
 
-def find_pet_by_name(pets_hash, pet_name)
-  pets = []
-  for pet in pets_hash[:pets]
-    if pet[:name] == pet_name
-      return pet
-    else
-      return nil
-    end
+def find_pet_by_name(pet_shop, pet_name)
+  match = nil
+  for pet in pet_shop[:pets]
+    match = pet if(pet[:name] == pet_name)
   end
+  return match
+end
+
+def remove_pet_by_name(pet_shop, pet_name)
+  pet_to_delete = find_pet_by_name(pet_shop, pet_name)
+  pet_shop[:pets].delete(pet_to_delete)
 end
 
 def add_pet_to_stock(pets_hash, add_pets_hash)
@@ -67,21 +66,17 @@ def add_pet_to_customer(customer, new_pet)
   customer[:pets] << new_pet
 end
 
-def customer_can_afford_pet(customer, new_pet_cost)
-  if customer[:cash] > new_pet_cost[:price]
-    return true
-    end
-  else
-    return false
+def customer_can_afford_pet(customer, pet)
+  return customer[:cash] >= pet[:price]
 end
 
-def sell_pet_to_customer(pet_shop_hash, pet, customer)
-  p pet
-  p customer
-  p pet_shop_hash[:pets]
-  customer[:pets] << pet_shop_hash[:pets][3]
-  p customer[:pets]
-  p customer[:pets].length
-  return customer[:pets].length
+def sell_pet_to_customer(pet_shop, pet, customer)
+  return if (pet == nil)
+  return if !(customer_can_afford_pet(customer, pet))
 
+  add_pet_to_customer(customer,pet)
+  add_or_remove_cash(pet_shop, pet[:price])
+  remove_customer_cash(pet_shop, pet[:price])
+  remove_pet_by_name(pet_shop, pet[:name])
+  increase_pets_sold(pet_shop, 1)
 end
